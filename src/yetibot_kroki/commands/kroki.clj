f043c80f8e885a4fd270d9f5526b9b6b63c354e4
Trevor Hartman <trevorhartman@gmail.com>
2020-10-20 10:23:00 -0600

    Add kroki types command

--- src/yetibot_kroki/commands/kroki.clj
+++ src/yetibot_kroki/commands/kroki.clj
@@ -21,8 +21,29 @@
   (str "https://demo.kroki.io/graphviz/png/"
        (encode-for-kroki graphviz)))
 
+(def diagram-types
+  {:blockdiag "BlockDiag"
+   :bpmn "BPMN"
+   :bytefield "Bytefield"
+   :seqdiag "SeqDiag"
+   :actdiag "ActDiag"
+   :nwdiag "NwDiag"
+   :packetdiag "PacketDiag"
+   :rackdiag "RackDiag"
+   :c4plantuml "C4 PlantUML"
+   :ditaa "Ditaa"
+   :erd "Erd"
+   :graphviz "GraphViz"
+   :mermaid "Mermaid"
+   :nomnoml "Nomnoml"
+   :plantuml "PlantUML"
+   :svgbob "Svgbob"
+   :vega "Vega"
+   :vegalite "Vega-Lite"
+   :wavedrom "WaveDrom"})
+
 (defn graphviz-cmd
-  "kroki graphviz <dot> # generate a graphviz graph"
+  "kroki <type> <graph> # generate a graphviz graph. Examples: https://kroki.io/examples.html"
   [{[_ graphviz] :match :as cmd}]
   (info "kroki" cmd)
   (graphviz-url graphviz))
@@ -31,6 +52,14 @@
 ;; - consider supporting all formats
 ;; - multi line commands
 
+(defn list-types-cmd
+  "kroki types # list types of supported diagrams"
+  [_]
+  {:result/value
+   (map (fn [[k v]] (format "`%s` - %s" (name k) v)) diagram-types)
+   :result/data diagram-types})
+
 (cmd-hook #"kroki"
-          #"graphviz\s+(.+)" graphviz-cmd)
+  #"types" list-types-cmd
+  #"graphviz\s+(.+)" graphviz-cmd)
 
